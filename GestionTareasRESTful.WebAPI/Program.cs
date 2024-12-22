b40c49b80dda5f5b61b26513f5301a5fc18d449d
Ariel20012002 <ariel20012002@gmail.com>
2024-12-22 16:48:53 -0500

    Se introduce los permisos para que API pueda darle servicio al FrontEnd a la parte web y movil.

--- GestionTareasRESTful.WebAPI/Program.cs
+++ GestionTareasRESTful.WebAPI/Program.cs
@@ -1,13 +1,18 @@
 using GestionTareasRESTful.Application;
+using GestionTareasRESTful.Domain.Repository;
 using GestionTareasRESTful.Infraestructure;
+using GestionTareasRESTful.Infraestructure.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpContextAccessor();
+
 // Add Layer Dependency
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +25,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(options =>
+{
+    options.AllowAnyHeader();
+    options.AllowAnyOrigin();
+    options.AllowAnyMethod();
+});
 
 app.UseHttpsRedirection();
 
